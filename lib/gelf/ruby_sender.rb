9c827a664d43a6215b2db8a8767c8e6b237fad8f
Edward De Faria <edward.de-faria@ovh.net>
2015-11-20 17:56:02 +0100

    Fix "NoMethodError: undefined method `match' for 0:Fixnum" in jruby

--- lib/gelf/ruby_sender.rb
+++ lib/gelf/ruby_sender.rb
@@ -208,12 +208,12 @@ module GELF
     end
 
     def send(message)
-      if @options['tcp_retry'] and @options['tcp_retry'].match(/^(\d)+$/)
+      if @options['tcp_retry'] and @options['tcp_retry'].is_i?
         max_retry = @options['tcp_retry'].to_i
       else
         max_retry = 0
       end
-      if @options['tcp_retry_ms'] and @options['tcp_retry_ms'].match(/^(\d)+$/)
+      if @options['tcp_retry_ms'] and @options['tcp_retry_ms'].is_i?
         sleep_retry = @options['tcp_retry_ms'].to_f
       else
         sleep_retry = 50
